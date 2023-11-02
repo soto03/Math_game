dbc34f9b72e9753daa61c42e50e50f6ecd690fd8
soto03 <diegosoto@liceovvh.cl>
2023-11-01 21:09:42 -0300

    se agrego la funcion "pregunta aleatoria"

--- Juego.cpp
+++ Juego.cpp
@@ -1,21 +1,51 @@
 #include <iostream>
+#include <string>
+#include <ctime>
 
 using namespace std;
 
+string pregunta_aleatoria(){
+	string suma = "+";
+	string resta = "-";
+	string multi = "*";
+	string divi = "/";
+	string ope[4] = { "+","-", "*","/"};
+
+	int semilla = time(NULL);
+	srand(semilla);
+
+	int cantidad_num = rand() % 4 + 2;
+	string pregunta = "";
+	
+	for(int i=0; i<cantidad_num; i++){
+		if( i == cantidad_num - 1){
+			pregunta = pregunta  + to_string(rand() % 20 + 1) + " ";
+		}
+		else{
+			pregunta = pregunta  + to_string(rand() % 20 + 1) + " " + ope[rand() % 3 + 0] + " ";
+		}
+	}
+	
+	
+	
+	return pregunta;
+}
+
 int main(){
+
 	int respuestas[3];
 	int correctas = 0;
 	
 	cout<<"Bienvenido a este pequeno desafio.\n";
 	cout<<"responde 3 preguntas para superarlo.\n";
 	
-	cout<<"cuanto es 3+5*2?\n";
+	cout<<"cuanto es " + pregunta_aleatoria() + "?\n";
 	cin>>respuestas[0];
 	
-	cout<<"cuanto es 3+5*2?\n";
+	cout<<"cuanto es " + pregunta_aleatoria() + "?\n";
 	cin>>respuestas[1];
 	
-	cout<<"cuanto es 3+5*2?\n";
+	cout<<"cuanto es " + pregunta_aleatoria() + "?\n";
 	cin>>respuestas[2];
 	
 	if(respuestas[0] == 13){
@@ -48,12 +78,6 @@ int main(){
 	}
 }
 
-char[] pregunta_aleatoria(){
-	
-	
-	
-	
-	return"";
-}
+
 
 
